32cd57aa314343cf3059e5f82fbdcaf7fce5e5cc
Zack Blaylock <zackblaylock@gmail.com>
2018-02-07 21:17:13 -0500

    Added UI design and shortened the the caption and title text.

--- _dist/post/render.js
+++ _dist/post/render.js
@@ -1,13 +1,24 @@
 export function PostResults(render) {
     let results = '';
+    let title = post.title;
+    let caption = post.caption;
+
+    if (title > 30) {
+        title = title.substring(0,29)+"...";
+    }
+
+    if (caption.length > 30) {
+        caption = caption.substring(0,29)+"...";
+    }
+
     render.forEach((post, index) => {
         results += `                
         <div id="${post.public_id}" class="post-card col-sm-4">
             <a class="ui card" data-toggle="modal" data-target="#Post-Page">
                 <image height="260" class="ui image" src="${post.secure_url}" />
                 <div class="content">
-                    <div class="header">${post.title}</div>
-                    <div class="caption">${post.caption}</div>
+                    <div class="header">${title}</div>
+                    <div class="caption">${caption}</div>
                     <div class="date">${post.created_at}</div>
                 </div>
             </a>
