14fb32e25a2eb3a0a1f191e47fb4b513d17c1425
Zack Blaylock <zackblaylock@gmail.com>
2018-02-07 21:31:50 -0500

    Added the bundle.js

--- public/bundle.js
+++ public/bundle.js
@@ -848,18 +848,19 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony export (immutable) */ __webpack_exports__["PostResults"] = PostResults;
 function PostResults(render) {
     let results = '';
-    let title = post.title;
-    let caption = post.caption;
-
-    if (title > 30) {
-        title = title.substring(0,29)+"...";
-    }
-
-    if (caption.length > 30) {
-        caption = caption.substring(0,29)+"...";
-    }
 
     render.forEach((post, index) => {
+        let title = post.title;
+        let caption = post.caption;
+
+        if (title > 30) {
+            title = title.substring(0,29)+"...";
+        }
+
+        if (caption.length > 30) {
+            caption = caption.substring(0,29)+"...";
+        }
+
         results += `                
         <div id="${post.public_id}" class="post-card col-sm-4">
             <a class="ui card" data-toggle="modal" data-target="#Post-Page">
