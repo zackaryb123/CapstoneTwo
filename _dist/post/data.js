9199d8a092e97060aa94c2ab1e6abcd78578b0d0
Zack Blaylock <zackblaylock@gmail.com>
2018-02-12 18:38:44 -0500

    Fixed the username storage after login

--- _dist/post/data.js
+++ _dist/post/data.js
@@ -1,9 +1,20 @@
 export function getUploadFormData(event) {
     const currentUser = JSON.parse(localStorage.getItem('currentUser')).username;
+    let title = $(event.currentTarget).find("input[name=title]").val();
+    let caption =  $(event.currentTarget).find("input[name=caption]").val();
+
+    if (title === null){
+        title = " ";
+    }
+
+    if (caption == null) {
+        caption = " ";
+    }
+
     return {
         username: currentUser,
-        title: $(event.currentTarget).find("input[name=title]").val(),
-        caption: $(event.currentTarget).find("input[name=caption]").val(),
+        title: title,
+        caption: caption,
         img: $(event.currentTarget).find("input[name=image]").val() 
     }
 
--- public/bundle.js
+++ public/bundle.js
@@ -887,10 +887,21 @@ function PostResults(render) {
 /* unused harmony export getUploadFormData */
 function getUploadFormData(event) {
     const currentUser = JSON.parse(localStorage.getItem('currentUser')).username;
+    let title = $(event.currentTarget).find("input[name=title]").val();
+    let caption =  $(event.currentTarget).find("input[name=caption]").val();
+
+    if (title === null){
+        title = " ";
+    }
+
+    if (caption == null) {
+        caption = " ";
+    }
+
     return {
         username: currentUser,
-        title: $(event.currentTarget).find("input[name=title]").val(),
-        caption: $(event.currentTarget).find("input[name=caption]").val(),
+        title: title,
+        caption: caption,
         img: $(event.currentTarget).find("input[name=image]").val() 
     }
 
